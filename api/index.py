ab79eb8a95c65748b58f79f141fbc08394532f61
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-19 14:53:19 +0200

    init again

--- api/index.py
+++ api/index.py
@@ -1,27 +1,48 @@
-from bottle import Bottle, run, get, post, request
+from bottle import Bottle, get, post, request
+from PIL import Image
+from io import BytesIO
+import base64
 
 app = Bottle()
 
-@app.route('/hello')
-def hello():
-    return "hello get route"
+class ImageUtils:
 
-@app.route('/')
-def hellotoo():
-    return "base route"
+    @staticmethod
+    def convertJPG(im):
+        with BytesIO() as f:
+           im = im.convert("RGB")
+           im.save(f, format='JPEG')
+           return base64.encodebytes(f.getvalue())
 
-@app.route('/login', method=['GET'])
-def login():
-    return '''
-        <form action="/login" method="post">
-            Username: <input name="username" type="text" />
-            Password: <input name="password" type="password" />
-            <input value="Login" type="submit" />
-        </form>
-    '''
+    @staticmethod
+    def convertPNG(im):
+        with BytesIO() as f:
+           im.save(f, format='PNG')
+           return base64.encodebytes(f.getvalue())
 
-# or @route('/login', method='POST')
-@app.route('/login', method=['POST'])
-def login():
-    body = request.json
-    return {'puppet': str(body)}
+@app.route("/imgcnv", methods=["POST"])
+def convert_image():
+    if request.json['image'] and request.json['to']:
+        format = request.json['to']
+        img = Image.open(BytesIO(base64.b64decode(request.json['image'])))
+        try:
+           return getattr(ImageUtils, "convert" + format.upper())(img)
+        except AttributeError as e:
+           return "Unsupported format: " + format , 400
+        except Exception as e:
+           print("Server crash" + str(e))
+           return "Server crash please report this error.", 400
+    else:
+        return "No image found", 400
+   
+@app.route("/imgd", methods=["POST"])
+def image_detail():
+    if request.json['image']:
+        try:
+            img = Image.open(BytesIO(base64.b64decode(request.json['image'])))
+            return jsonify({'msg': 'success', 'size': [img.width, img.height], 'mode': img.mode, 'format': img.format}) 
+        except Exception as e:
+            print("Server crash" + str(e))
+            return "Server crash please report this error.", 400
+    else:
+        return "No image found", 400
