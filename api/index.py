36f92c0a2606e6e92a0875cc0a66b0ee61085d53
Raiyan Yahya <raiyanyahyadeveloper@gmail.com>
2021-08-19 22:50:43 +0200

    test

--- api/index.py
+++ api/index.py
@@ -24,6 +24,23 @@ class ImageUtils:
 def return_response(msg, code):
     response.status = code
     return json.dumps({"message": msg})
+# This route capture all route for method OPTIONS
+@bottle.route('/<:re:.*>', method='OPTIONS')
+def cors():
+    pass
+
+headers = ['Origin', 'Accept', 'Content-Type',
+           'X-Requested-With', 'X-CSRF-Token',
+           'Authorization']
+HEADERS = ', '.join((headers + [h.lower() for h in headers]))
+
+# For all request I add cors headers
+def apply_cors():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, DELETE, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = HEADERS
+
+app.add_hook('after_request', apply_cors)
 
 @app.route("/", methods=["POST","GET"])
 def ping():
